d300cec9a5f93ac77111146d8c1bcd0b766ac063
AnnaGo-cf <anna.goncharova@confirmit.com>
2022-10-04 14:54:21 +0400

    chore: lint html

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,21 @@ module.exports = {
     jquery: true,
   },
   extends: ['airbnb-base', 'prettier'],
-  overrides: [],
+  overrides: [
+    {
+      files: ['*.html'],
+      parser: '@html-eslint/parser',
+      extends: ['plugin:@html-eslint/recommended'],
+      rules: {
+        'spaced-comment': 0,
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {},
-  ignorePatterns: ['src/template/*', 'webpack.*.js'],
+  plugins: ['html', '@html-eslint'],
+  ignorePatterns: ['src/template/*', 'webpack.*.js', 'dist'],
 };
